41ef88533e0d857e78d5a727e2c2b8a969c2e519
HYDRA MARTÍNEZ YEGUAS <Hydra.Martinez@alu.uclm.es>
2022-11-24 14:06:11 +0100

    Creamos otra imagen para mongo

--- deploy/docker/Order/drop/run.sh
+++ deploy/docker/Order/drop/run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#no lanzar el .jar hasta que no este accesible el servicio de MogoDB
+#el host 'mongo' tendrá una ip otorgada al lanzar la pila de servicios,
+#y le damos el apodo 'mongo' utilizado aqui y en el archivo application.properties
 
 while ! curl http://mongo:27017/
 do
@@ -7,4 +10,4 @@ do
 done
 echo "$(date) - connected successfully"
 
-java -jar ordering-*.jar
+java -jar usr/local/app/ordering-service-0.1.0.jar -spring.config.location=application.properties
